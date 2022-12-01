45bc3509f4d4ceeaa2442a62e88d6ebc7a091522
Vivek <vm800@york.ac.uk>
2022-12-01 16:01:24 +0000

    Advent of Code Day 1 Part 2

--- Day_One_Calorie_Counting.py
+++ Day_One_Calorie_Counting.py
@@ -1,7 +1,15 @@
-def calorie_counting(puzzle_input):
+def calorie_counting_max(puzzle_input):
     cals_by_elves = puzzle_input.split("\n\n")
     total_cals_per_elves = []
-    print(cals_by_elves)
     for cals_by_elf in cals_by_elves:
         total_cals_per_elves.append(sum([int(x) for x in cals_by_elf.split("\n")]))
     return max(total_cals_per_elves)
+
+
+def calorie_counting_top_three(puzzle_input):
+    cals_by_elves = puzzle_input.split("\n\n")
+    total_cals_per_elves = []
+    for cals_by_elf in cals_by_elves:
+        total_cals_per_elves.append(sum([int(x) for x in cals_by_elf.split("\n")]))
+    total_cals_per_elves.sort(reverse=True)
+    return total_cals_per_elves[0] + total_cals_per_elves[1] + total_cals_per_elves[2]
